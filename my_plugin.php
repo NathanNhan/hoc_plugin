c5592683e22eacc076953a5a811517dd7b1238c0
Lê Trọng Nhân <44769901+NathanNhan@users.noreply.github.com>
2024-03-13 15:32:36 +0700

    first commit

---
+++ my_plugin.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*
+ * Plugin Name:       My Plugin
+ * Plugin URI:        https://trongnhandev.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Trong Nhan
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-plugin
+ * Domain Path:       /languages
+ */
+
+defined("ABSPATH") or die("You can not access directly");
