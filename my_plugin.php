e8601819409be20ddbe9c4f87be30486b990261f
Lê Trọng Nhân <44769901+NathanNhan@users.noreply.github.com>
2024-03-15 20:26:00 +0700

    bài 1: admin_menu hook

--- my_plugin.php
+++ my_plugin.php
@@ -17,3 +17,34 @@
  */
 
 defined("ABSPATH") or die("You can not access directly");
+
+//admin_menu hook
+// Design patterns -> SingleTon
+if(!class_exists('MyPlugin')) {
+  class MyPlugin {
+  public function __construct() {
+   add_action( 'admin_menu', array($this, 'custom_admin_menu') );
+  }
+
+  function custom_admin_menu() {
+    add_menu_page( 'All Employees', 'All Employess', 'manage_options', 'all-employees', array($this, 'render_employee'), '', 10 );
+    add_submenu_page( 'all-employees', 'Add Employees', 'Add Employees', 'manage_options', 'add-employee', array($this, 'render_add_employee'), 1 );
+  }
+  //List Employees
+  function render_employee() {
+    ?> 
+      <h1>List Employees</h1>
+    <?php 
+  }
+  //Add Employee 
+  function render_add_employee() {
+    ?> 
+      <h1>Add Employee</h1>
+    <?php 
+  }
+ 
+}
+}
+
+// $plugins = new MyPlugin();
+new MyPlugin;
